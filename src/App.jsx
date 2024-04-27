a3bec99b56a253f8f89ab8b735dddca228b0eac8
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-04-27 16:45:09 +0600

    home page ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 
 import './App.css'
+import LetsGoWithUs from './Components/LetsGoWithUs'
+import OurPartners from './Components/OurPartners'
+import QuickAns from './Components/QuickAns'
+import RecommendedBestPlaces from './Components/RecommendedBestPlaces'
 import Slider from './Components/Slider/Slider'
 
 
@@ -9,7 +13,21 @@ function App() {
   return (
     <>
       <div className='m-10'>
-        <Slider></Slider>
+        <Slider />
+      </div>
+
+      <div>
+        <RecommendedBestPlaces />
+      </div>
+
+      <div>
+        <QuickAns />
+      </div>
+      <div>
+        <LetsGoWithUs></LetsGoWithUs>
+      </div>
+      <div>
+        <OurPartners />
       </div>
     </>
   )
